5b8bda445f47d1adc61acc67848e412c69c391cf
masafumi330 <mihusamawasami@gmail.com>
2024-11-10 00:33:07 +0900

    Update, Deleteのテストコードも書く

--- app/internal/usecase/todo_usecase_test.go
+++ app/internal/usecase/todo_usecase_test.go
@@ -61,3 +61,77 @@ func TestTodoUsecase_CreateTodo_Success(t *testing.T) {
 	}
 	assert.Equal(t, 1, count, "expected one record in the todos table")
 }
+
+func TestTodoUsecase_UpdateTodo_Success(t *testing.T) {
+	// Arrange
+	// テストが終わったら毎回ロールバックする
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatalf("failed to begin transaction: %v", err)
+	}
+	t.Cleanup(func() {
+		tx.Rollback()
+	})
+	repo := repository.NewTodoRepository(tx)
+	usecase := NewTodoUsecase(repo)
+	result, err := tx.Exec("INSERT INTO todos (title, done) VALUES (?, ?)", "Test Todo", false)
+	if err != nil {
+		t.Fatalf("failed to insert todo: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		t.Fatalf("failed to get last insert id: %v", err)
+	}
+
+	// Act
+	err = usecase.UpdateTodo(uint(id), "Updated Todo", true)
+	if err != nil {
+		t.Errorf("failed to create todo: %v", err)
+	}
+
+	// Assert
+	var title string
+	var done bool
+	err = tx.QueryRow("SELECT title, done FROM todos WHERE id = ?", id).Scan(&title, &done)
+	if err != nil {
+		t.Fatalf("failed to query todos table: %v", err)
+	}
+	assert.Equal(t, "Updated Todo", title, "expected title to be Updated Todo")
+	assert.True(t, done, "expected done to be true")
+}
+
+func TestTodoUsecase_DeleteTodo_Success(t *testing.T) {
+	// Arrange
+	// テストが終わったら毎回ロールバックする
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatalf("failed to begin transaction: %v", err)
+	}
+	t.Cleanup(func() {
+		tx.Rollback()
+	})
+	repo := repository.NewTodoRepository(tx)
+	usecase := NewTodoUsecase(repo)
+	result, err := tx.Exec("INSERT INTO todos (title, done) VALUES (?, ?)", "Test Todo", false)
+	if err != nil {
+		t.Fatalf("failed to insert todo: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		t.Fatalf("failed to get last insert id: %v", err)
+	}
+
+	// Act
+	err = usecase.DeleteTodo(uint(id))
+	if err != nil {
+		t.Errorf("failed to create todo: %v", err)
+	}
+
+	// Assert
+	var count int
+	err = tx.QueryRow("SELECT COUNT(*) FROM todos WHERE id = ?", id).Scan(&count)
+	if err != nil {
+		t.Fatalf("failed to query todos table: %v", err)
+	}
+	assert.Equal(t, 0, count, "expected no record in the todos table")
+}
